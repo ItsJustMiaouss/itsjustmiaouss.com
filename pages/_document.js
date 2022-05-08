f21c833f8a9fa4886642148122e84ae09cc835fa
ItsJustMiaouss <roman.doyelle@gmail.com>
2022-05-08 02:13:42 +0200

    feat: eslint & prettier

--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,15 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <meta charSet="UTF-8" />
-        <meta name="description" content="I'm ItsJustMiaouss, a passionate developer from France! Visit my website and send me a message!" />
+        <meta
+          name="description"
+          content="I'm ItsJustMiaouss, a passionate developer from France! Visit my website and send me a message!"
+        />
         <meta property="og:type" content="website" />
-        <meta property="og:description" content="I'm ItsJustMiaouss, a passionate developer from France! Visit my website and send me a message!" />
+        <meta
+          property="og:description"
+          content="I'm ItsJustMiaouss, a passionate developer from France! Visit my website and send me a message!"
+        />
         <meta property="og:site_name" content="ItsJustMiaouss" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <Head />
