f48c4b3c4b9091fd66e33a2d113c92de1809801c
ItsJustMiaouss <59478524+ItsJustMiaouss@users.noreply.github.com>
2022-08-27 03:35:14 +0200

    feat: updated main page style

--- pages/index.js
+++ pages/index.js
@@ -16,21 +16,25 @@ export default function Home() {
               width={250}
               height={250}></Image>
 
-            <p className="text-gray-300 text-3xl  md:text-4xl mt-6">
-              Hi, I'm <i className="twa twa-wave"></i>
-            </p>
             <h1 className="text-4xl md:text-6xl">ItsJustMiaouss</h1>
-            <h2 className="text-gray-300 mt-2 mb-3 text-base hidden md:block">
-              I'm a passionate developer from France{' '}
-              <i className="twa twa-fr"></i>!
+            <h2 className="text-gray-300 mt-2 mb-3 text-base md:text-xl">
+              I'm a passionate developer from France <i className="twa twa-fr"></i>!
             </h2>
-            <div className="text-gray-300 hover:opacity-60 text-2xl md:text-base cursor-pointer w-fit m-auto">
+
+            <div className="space-x-6">
+              <Link href="/projects">
+                <button className="cursor-pointer border-2 border-blue-700 bg-blue-700 px-8 py-2 rounded-xl hover:opacity-60 duration-300">
+                  <i className="far fa-box"></i> Projects
+                </button>
+              </Link>
+              
               <Link href="/about">
-                <a title="About me" className="text-gray-300 underline">
-                  About me <i className="far fa-arrow-right"></i>
-                </a>
+                <button className="cursor-pointer border-2 px-8 py-2 rounded-xl hover:opacity-60 duration-300">
+                  <i className="far fa-user"></i> About me
+                </button>
               </Link>
             </div>
+
           </div>
         </div>
       </Layout>
