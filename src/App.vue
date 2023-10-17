006ee93b8a1d13c8b378b3b61ab96ada3409c1b3
ItsJustMiaouss <59478524+ItsJustMiaouss@users.noreply.github.com>
2023-10-17 23:36:01 +0200

    fix: footer text alignment on small screens

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,12 @@ import SocialIcon from "./components/SocialIcon.vue"
       <div
         class="flex flex-col justify-center items-center align-middle m-auto h-full text-center font-sora">
         <!-- Profile picture -->
-        <img class="rounded-lg" width="200" height="200" src="./assets/profile.png" alt="Profile" />
+        <img
+          class="rounded-lg"
+          width="200"
+          height="200"
+          src="./assets/profile.png"
+          alt="Profile" />
         <!-- Title -->
         <h1 class="text-4xl md:text-5xl mt-4">ItsJustMiaouss</h1>
         <!-- Description -->
@@ -27,19 +32,19 @@ import SocialIcon from "./components/SocialIcon.vue"
         <!-- Social links -->
         <div class="flex space-x-6 mt-2">
           <SocialIcon
-						title="Discord"
+            title="Discord"
             icon="fa-brands fa-discord"
             link="https://discordapp.com/users/254342535172849674" />
           <SocialIcon
-						title="GitHub"
+            title="GitHub"
             icon="fa-brands fa-github"
             link="https://github.com/ItsJustMiaouss" />
           <SocialIcon
-						title="Twitter"
+            title="Twitter"
             icon="fa-brands fa-twitter"
             link="https://twitter.com/ItsJustMiaouss" />
           <SocialIcon
-						title="Steam"
+            title="Steam"
             icon="fa-brands fa-steam"
             link="https://steamcommunity.com/id/ItsJustMiaouss" />
         </div>
@@ -47,7 +52,7 @@ import SocialIcon from "./components/SocialIcon.vue"
     </body>
 
     <!-- Footer -->
-    <footer>
+    <footer class="flex align-middle justify-center md:justify-start">
       <span class="opacity-60 font-light">
         Made with
         <i class="twa twa-heart-suit"></i>
