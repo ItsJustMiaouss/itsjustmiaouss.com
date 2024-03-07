aca0220fa8609c1ea8b7480a92197d435811d140
ItsJustMiaouss <59478524+ItsJustMiaouss@users.noreply.github.com>
2024-03-07 02:37:11 +0100

    add: something

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,57 @@
 <script setup>
+import { ref } from "vue"
 import SocialIcon from "./components/SocialIcon.vue"
+import JSConfetti from "js-confetti"
+
+const clickCount = ref(0)
+
+const animate = ref(false)
+const animationStarted = ref(false)
+
+const jsConfetti = new JSConfetti()
+
+console.log("Hint: Cats have 9 lives.")
+
+function incrementClickCount() {
+  clickCount.value += 1
+
+  if (clickCount.value >= 9) {
+    startAnimation()
+    clickCount.value = 0
+  }
+}
+
+function startAnimation() {
+  if (animationStarted.value) return
+
+  animationStarted.value = true
+
+  const audio = new Audio("/audio.wav")
+  audio.play()
+
+  jsConfetti.addConfetti()
+
+  setTimeout(() => {
+    animate.value = true
+
+    jsConfetti.addConfetti({
+      emojis: ["🐱", "😹", "😻", "🙀"],
+      emojiSize: 40,
+      confettiNumber: 30,
+    })
+
+    const loop = setInterval(() => {
+      jsConfetti.addConfetti()
+    }, 1800)
+
+    // When the music ends
+    setTimeout(() => {
+      clearInterval(loop)
+      animate.value = false
+      animationStarted.value = false
+    }, 62700)
+  }, 4000)
+}
 </script>
 
 <template>
@@ -17,25 +69,37 @@ import SocialIcon from "./components/SocialIcon.vue"
         <!-- Profile picture -->
         <div>
           <img
-            class="rounded-lg m-auto"
+            class="rounded-lg m-auto hover:opacity-60 duration-150"
+            :class="{ 'animate-danse': animate }"
+            @click="incrementClickCount"
             width="200"
             height="200"
             src="./assets/profile.png"
             alt="Profile" />
 
           <!-- Paw icon -->
-          <i class="paw-icon mr-[60vw] rotate-[30deg] -mt-10 hidden sm:block" />
+          <i
+            class="paw-icon mr-[60vw] rotate-[30deg] -mt-10 hidden sm:block"
+            :class="{ 'animate-roll': animate }" />
         </div>
         <!-- Title -->
-        <h1 class="text-4xl md:text-5xl mt-4">ItsJustMiaouss</h1>
+        <h1
+          class="text-4xl md:text-5xl mt-4"
+          :class="{ 'animate-move-right': animate }">
+          ItsJustMiaouss
+        </h1>
         <!-- Description -->
-        <p class="text-gray-300 text-sm md:text-md md:text-xl font-extralight">
+        <p
+          class="text-gray-300 text-sm md:text-md md:text-xl font-extralight"
+          :class="{ 'animate-move-left': animate }">
           I'm a passionate developer from France
           <i class="twa twa-flag-france"></i>
           !
         </p>
         <!-- Social links -->
-        <div class="flex space-x-6 mt-2">
+        <div
+          class="flex space-x-6 mt-2"
+          :class="{ 'animate-move-left': animate }">
           <SocialIcon
             title="Discord"
             icon="fa-brands fa-discord"
@@ -55,7 +119,9 @@ import SocialIcon from "./components/SocialIcon.vue"
         </div>
 
         <!-- Paw icon -->
-        <i class="paw-icon ml-[60vw] rotate-[-25deg] hidden sm:block" />
+        <i
+          class="paw-icon ml-[60vw] rotate-[-25deg] hidden sm:block"
+          :class="{ 'animate-roll': animate }" />
       </div>
     </body>
 
