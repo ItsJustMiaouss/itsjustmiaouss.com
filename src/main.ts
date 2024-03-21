7a793658e08af71c68a231ee586d486d33b024b3
ItsJustMiaouss <59478524+ItsJustMiaouss@users.noreply.github.com>
2024-03-21 23:20:37 +0100

    feat: add vue router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,27 @@
 import { createApp } from "vue"
 import "./style.css"
 import App from "./App.vue"
+import { createRouter, createWebHistory } from "vue-router"
+import Home from "./components/Home.vue"
 
-createApp(App).mount("#app")
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    name: "Discord",
+    path: "/discord",
+    component: {},
+    beforeEnter: () => {
+      location.href = "https://discord.gg/b6PNpxnWZY"
+    },
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount("#app")
